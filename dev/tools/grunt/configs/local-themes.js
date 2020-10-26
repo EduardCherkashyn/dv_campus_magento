a2b63294bf1a1c80903fec5ad9d2e230c06836a8
Eduard Cherkashyn <edikparker@gmail.com>
2020-10-26 21:48:07 +0200

    #4 Create Grunt config files for theme EduardCherkashyn/luma

---
+++ dev/tools/grunt/configs/local-themes.js
@@ -0,0 +1,15 @@
+/*
+ *  grunt exec:eduardcherkashyn_luma_en_us && grunt less:eduardcherkashyn_luma_en_us
+ */
+module.exports = {
+    eduardcherkashyn_luma_en_us: {
+        area: 'frontend',
+        name: 'EduardCherkashyn/luma',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+};
